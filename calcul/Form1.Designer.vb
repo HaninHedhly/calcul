acb9cd60747a7dae8e4f17cbebf6ab392af43658
Hanin <Hanin@DESKTOP-5TR19M0>
2024-11-04 10:20:25 +0100

    Ajoutez des fichiers projet.

---
+++ calcul/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        un = New Button()
+        deux = New Button()
+        trois = New Button()
+        quatre = New Button()
+        cinq = New Button()
+        six = New Button()
+        sept = New Button()
+        eight = New Button()
+        night = New Button()
+        zero = New Button()
+        plus = New Button()
+        moin = New Button()
+        fois = New Button()
+        division = New Button()
+        resultat = New Button()
+        txt_affiche = New TextBox()
+        C = New Button()
+        SuspendLayout()
+        ' 
+        ' un
+        ' 
+        un.AccessibleName = "b1"
+        un.Location = New Point(57, 83)
+        un.Name = "un"
+        un.Size = New Size(75, 32)
+        un.TabIndex = 4
+        un.Text = "1"
+        un.UseVisualStyleBackColor = True
+        ' 
+        ' deux
+        ' 
+        deux.AccessibleName = "b2"
+        deux.Location = New Point(172, 83)
+        deux.Name = "deux"
+        deux.Size = New Size(75, 32)
+        deux.TabIndex = 5
+        deux.Text = "2"
+        deux.UseVisualStyleBackColor = True
+        ' 
+        ' trois
+        ' 
+        trois.AccessibleName = "b3"
+        trois.Location = New Point(269, 83)
+        trois.Name = "trois"
+        trois.Size = New Size(75, 32)
+        trois.TabIndex = 6
+        trois.Text = "3"
+        trois.UseVisualStyleBackColor = True
+        ' 
+        ' quatre
+        ' 
+        quatre.AccessibleName = "b4"
+        quatre.Location = New Point(57, 121)
+        quatre.Name = "quatre"
+        quatre.Size = New Size(75, 33)
+        quatre.TabIndex = 7
+        quatre.Text = "4"
+        quatre.UseVisualStyleBackColor = True
+        ' 
+        ' cinq
+        ' 
+        cinq.AccessibleName = "b5"
+        cinq.Location = New Point(172, 121)
+        cinq.Name = "cinq"
+        cinq.Size = New Size(75, 33)
+        cinq.TabIndex = 8
+        cinq.Text = "5"
+        cinq.UseVisualStyleBackColor = True
+        ' 
+        ' six
+        ' 
+        six.AccessibleName = "b6"
+        six.Location = New Point(269, 121)
+        six.Name = "six"
+        six.Size = New Size(75, 33)
+        six.TabIndex = 9
+        six.Text = "6"
+        six.UseVisualStyleBackColor = True
+        ' 
+        ' sept
+        ' 
+        sept.AccessibleName = "b7"
+        sept.Location = New Point(57, 162)
+        sept.Name = "sept"
+        sept.Size = New Size(75, 31)
+        sept.TabIndex = 10
+        sept.Text = "7"
+        sept.UseVisualStyleBackColor = True
+        ' 
+        ' eight
+        ' 
+        eight.AccessibleName = "b8"
+        eight.Location = New Point(172, 162)
+        eight.Name = "eight"
+        eight.Size = New Size(75, 31)
+        eight.TabIndex = 11
+        eight.Text = "8"
+        eight.UseVisualStyleBackColor = True
+        ' 
+        ' night
+        ' 
+        night.AccessibleName = "b9"
+        night.Location = New Point(269, 162)
+        night.Name = "night"
+        night.Size = New Size(75, 31)
+        night.TabIndex = 12
+        night.Text = "9"
+        night.UseVisualStyleBackColor = True
+        ' 
+        ' zero
+        ' 
+        zero.AccessibleName = "b10"
+        zero.Location = New Point(172, 199)
+        zero.Name = "zero"
+        zero.Size = New Size(75, 35)
+        zero.TabIndex = 13
+        zero.Text = "0"
+        zero.UseVisualStyleBackColor = True
+        ' 
+        ' plus
+        ' 
+        plus.AccessibleName = "plus"
+        plus.Location = New Point(417, 87)
+        plus.Name = "plus"
+        plus.Size = New Size(75, 49)
+        plus.TabIndex = 14
+        plus.Text = "+"
+        plus.UseVisualStyleBackColor = True
+        ' 
+        ' moin
+        ' 
+        moin.AccessibleName = "moin"
+        moin.Location = New Point(541, 83)
+        moin.Name = "moin"
+        moin.Size = New Size(75, 49)
+        moin.TabIndex = 15
+        moin.Text = "-"
+        moin.UseVisualStyleBackColor = True
+        ' 
+        ' fois
+        ' 
+        fois.AccessibleName = "fois"
+        fois.Location = New Point(417, 162)
+        fois.Name = "fois"
+        fois.Size = New Size(75, 47)
+        fois.TabIndex = 16
+        fois.Text = "*"
+        fois.UseVisualStyleBackColor = True
+        ' 
+        ' division
+        ' 
+        division.AccessibleDescription = "div"
+        division.Location = New Point(541, 162)
+        division.Name = "division"
+        division.Size = New Size(75, 47)
+        division.TabIndex = 17
+        division.Text = "/"
+        division.UseVisualStyleBackColor = True
+        ' 
+        ' resultat
+        ' 
+        resultat.AccessibleName = "equ"
+        resultat.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        resultat.ForeColor = SystemColors.ActiveCaptionText
+        resultat.Location = New Point(417, 29)
+        resultat.Name = "resultat"
+        resultat.Size = New Size(75, 31)
+        resultat.TabIndex = 20
+        resultat.Text = "="
+        resultat.UseVisualStyleBackColor = True
+        ' 
+        ' txt_affiche
+        ' 
+        txt_affiche.Location = New Point(57, 29)
+        txt_affiche.Name = "txt_affiche"
+        txt_affiche.Size = New Size(294, 23)
+        txt_affiche.TabIndex = 21
+        ' 
+        ' C
+        ' 
+        C.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        C.Location = New Point(541, 29)
+        C.Name = "C"
+        C.Size = New Size(75, 31)
+        C.TabIndex = 22
+        C.Text = "C"
+        C.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Pink
+        ClientSize = New Size(800, 450)
+        Controls.Add(C)
+        Controls.Add(txt_affiche)
+        Controls.Add(resultat)
+        Controls.Add(division)
+        Controls.Add(fois)
+        Controls.Add(moin)
+        Controls.Add(plus)
+        Controls.Add(zero)
+        Controls.Add(night)
+        Controls.Add(eight)
+        Controls.Add(sept)
+        Controls.Add(six)
+        Controls.Add(cinq)
+        Controls.Add(quatre)
+        Controls.Add(trois)
+        Controls.Add(deux)
+        Controls.Add(un)
+        Name = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents un As Button
+    Friend WithEvents deux As Button
+    Friend WithEvents trois As Button
+    Friend WithEvents quatre As Button
+    Friend WithEvents cinq As Button
+    Friend WithEvents six As Button
+    Friend WithEvents sept As Button
+    Friend WithEvents eight As Button
+    Friend WithEvents night As Button
+    Friend WithEvents zero As Button
+    Friend WithEvents plus As Button
+    Friend WithEvents moin As Button
+    Friend WithEvents fois As Button
+    Friend WithEvents division As Button
+
+    Friend WithEvents resultat As Button
+    Friend WithEvents txt_affiche As TextBox
+    Friend WithEvents C As Button
+
+End Class
---
+++ calcul/Form1.vb
@@ -0,0 +1,159 @@
+﻿Public Class Form1
+    Dim A As String
+    Dim B As String
+    Dim op = ""
+
+    Private Sub zero_Click(sender As Object, e As EventArgs) Handles zero.Click
+        If op = "" Then
+            A = A + "0"
+        Else
+            B = B + "0"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+
+    End Sub
+
+    Private Sub un_Click(sender As Object, e As EventArgs) Handles un.Click
+        If op = "" Then
+            A = A + "1"
+        Else
+            B = B + "1"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub deux_Click(sender As Object, e As EventArgs) Handles deux.Click
+        If op = "" Then
+            A = A + "2"
+        Else
+            B = B + "2"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub trois_Click(sender As Object, e As EventArgs) Handles trois.Click
+        If op = "" Then
+            A = A + "3"
+        Else
+            B = B + "3"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub quatre_Click(sender As Object, e As EventArgs) Handles quatre.Click
+        If op = "" Then
+            A = A + "4"
+        Else
+            B = B + "4"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub cinq_Click(sender As Object, e As EventArgs) Handles cinq.Click
+        If op = "" Then
+            A = A + "5"
+        Else
+            B = B + "5"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub six_Click(sender As Object, e As EventArgs) Handles six.Click
+        If op = "" Then
+            A = A + "6"
+        Else
+            B = B + "6"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub sept_Click(sender As Object, e As EventArgs) Handles sept.Click
+        If op = "" Then
+            A = A + "7"
+        Else
+            B = B + "7"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub eight_Click(sender As Object, e As EventArgs) Handles eight.Click
+        If op = "" Then
+            A = A + "8"
+        Else
+            B = B + "8"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub night_Click(sender As Object, e As EventArgs) Handles night.Click
+        If op = "" Then
+            A = A + "9"
+        Else
+            B = B + "9"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub plus_Click(sender As Object, e As EventArgs) Handles plus.Click
+        If Not A = "" Then
+            op = "+"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub moin_Click(sender As Object, e As EventArgs) Handles moin.Click
+        If Not A = "" Then
+            op = "-"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub fois_Click(sender As Object, e As EventArgs) Handles fois.Click
+        If Not A = "" Then
+            op = "*"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub division_Click(sender As Object, e As EventArgs) Handles division.Click
+        If Not A = "" Then
+            op = "/"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub C_Click(sender As Object, e As EventArgs) Handles C.Click
+        txt_affiche.Text = ""
+
+    End Sub
+
+    Private Sub resultat_Click(sender As Object, e As EventArgs) Handles resultat.Click
+        Dim test = True
+        Dim res As Double
+        If IsNumeric(A) And IsNumeric(B) And Not op = "" Then
+            Dim va = Integer.Parse(A)
+            Dim vb = Integer.Parse(B)
+            If op = "+" Then
+                res = va + vb
+            ElseIf op = "-" Then
+                res = va - vb
+            ElseIf op = "*" Then
+                res = va * vb
+            ElseIf op = "/" And Not vb = "" Then
+                res = va / vb
+            Else
+                MessageBox.Show("div par 0")
+                test = False
+
+            End If
+        Else
+            MessageBox.Show("A et B doivent etre des entiers")
+            test = False
+
+        End If
+        If test Then
+            txt_affiche.Text = txt_affiche.Text + "=" + res.ToString
+
+        End If
+    End Sub
+End Class
